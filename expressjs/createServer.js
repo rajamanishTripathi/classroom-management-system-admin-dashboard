5a73f8b080b8c16baf1db73713b05cf425d559fa
Raja Manish <raghupatiraghav882@gmail.com>
2026-02-18 14:27:11 +0530

    setup postgres neon serverless database

--- expressjs/createServer.js
+++ expressjs/createServer.js
@@ -5,6 +5,7 @@ import express from 'express';
 const app = express(); // new instance of express application
 const port = 3000;
 
+const router = express.Router();
 // const server = http.createServer((req,res) =>{
 //     res.writeHead(200, {'Content-type':'text/plain'});
 //     res.end('Server is set up');
@@ -15,5 +16,39 @@ const port = 3000;
 app.get('/', (req,res) =>{
     res.send('Hello There');
 });
+
+// to read json data
+app.use(express.json());
+
+// app.get('/api/v1/cars', (req,res) =>{
+//     res.send('Hello There');
+// });
+
+// app.get('/api/v1/cars', (req,res) =>{
+//     res.send('Hello There');
+// });
+// app.post('/api/v1/cars', (req,res) =>{
+//     res.send('Hello There');
+// });
+// app.put('/api/v1/cars', (req,res) =>{
+//     res.send('Hello There');
+// });
+
+router.get('/cars', (req,res) =>{
+    res.send('Hello There');
+});
+
+router.get('/cars', (req,res) =>{
+    res.send('Hello There');
+});
+router.post('/cars', (req,res) =>{
+    res.send('Hello There');
+});
+router.put('/cars', (req,res) =>{
+    res.send('Hello There');
+});
+
+app.use('/api/v1/', router);
+
 
 app.listen(port , () => {console.log(`Listening to http://localhost:${port}`)});
---
+++ postgresql/README.md
@@ -0,0 +1,6 @@
+npm install drizzle-orm @neondatabase/serverless dotenv
+npm install -D drizzle-kit
+
+
+Neon Docs
+https://neon.com/docs/guides/drizzle
